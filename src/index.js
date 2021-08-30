dd215bad23dc7c46bcf4f34e2838d8001cad5764
Denisovitch <denisglazin@gmail.com>
2021-08-30 21:04:21 +0300

    init- start task 'brackers'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let res = false;
+    let s_str = str;
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let brackEr = bracketsConfig[j][0] + bracketsConfig[j][1];
+            s_str = s_str.replace(brackEr, "");
+        }
+    }
+    if (s_str == 0) {
+        res = true;
+    }
+
+    return res;
+};
+
+// console.log(check("()", [["(", ")"]])); // -> верно
+// console.log(
+//     check("| () |", [
+//         ["(", ")"],
+//         ["|", " | "],
+//     ])
+// ); // -> true
+
+// // "()"       "(", ")"   // -> верно
